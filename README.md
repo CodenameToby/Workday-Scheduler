2d93862c1721bb78663efc99e2913207cce120be
Toby Warburton <tobywarburton10@gmail.com>
2023-06-20 12:54:05 -0700

    Added HTML, CSS and jQuery to get a working application. Still need to work on spacing of each hour block

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Workday-Scheduler
 Day planner to help organize a busy day
+
+## How it works
+The planner will have the current day displayed at the top of a calender
+The user will be able to add events to existing timeblocks for standard 9am - 5pm hours.
+When the user clicks in a timeblock, they can enter information about the event.
+That text will be saved in local storage and the events will persist when the apge is refreshed.
+
+## Screenshot 
+
+
+## Link to delpoyed site
+
+
+
---
+++ assets/script.js
@@ -0,0 +1,65 @@
+// Main driver of webpage.
+// Display current date at the top of the page.
+// Add a function to the page for each clickable hour of the workday.
+// That function saves the info input to local storage when the save button is clicked.
+// Then the info input is displayed when the page is refeshed.
+
+$(document).ready(function() {
+    // Display current day at the top of the calendar
+    var currentDay = dayjs().format('dddd, MMMM D, YYYY');
+    $('#currentDay').text(currentDay);
+  
+    // Generate timeblocks for standard business hours
+    var startTime = 9;
+    var endTime = 17;
+    var container = $('.container');
+  
+    for (var hour = startTime; hour <= endTime; hour++) {
+      var timeblock = $('<div>').addClass('timeblock');
+      var hourCol = $('<div>').addClass('hour').text(hour + ':00');
+      var eventInput = $('<textarea>').addClass('event-input');
+      var saveBtn = $('<button>').addClass('save-btn').text('Save');
+  
+      timeblock.append(hourCol, eventInput, saveBtn);
+      container.append(timeblock);
+    }
+  
+    // Color code timeblocks based on past, present, or future
+    function updateTimeblockColors() {
+      var currentHour = dayjs().hour();
+  
+      $('.timeblock').each(function() {
+        var hour = parseInt($(this).find('.hour').text());
+        if (hour < currentHour) {
+          $(this).removeClass('present future').addClass('past');
+        } else if (hour === currentHour) {
+          $(this).removeClass('past future').addClass('present');
+        } else {
+          $(this).removeClass('past present').addClass('future');
+        }
+      });
+    }
+  
+    updateTimeblockColors();
+  
+    // Save event text to local storage
+    $('.save-btn').on('click', function() {
+      var eventText = $(this).siblings('.event-input').val();
+      var hour = $(this).siblings('.hour').text();
+  
+      localStorage.setItem(hour, eventText);
+    });
+  
+    // Load saved events from local storage
+    function loadSavedEvents() {
+      $('.timeblock').each(function() {
+        var hour = $(this).find('.hour').text();
+        var eventText = localStorage.getItem(hour);
+  
+        $(this).find('.event-input').val(eventText);
+      });
+    }
+  
+    loadSavedEvents();
+  });
+  
